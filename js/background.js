9bbf9a58c100bfece2611f6c7714d5fa1ad34721
RichoHan <Richo@RichoHandeMac-mini.local>
2014-04-26 20:45:18 +0800

    Add page

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript({file: "js/jquery-2.1.0.min.js"},function () {
+		chrome.tabs.insertCSS({file: "stylesheets/styles.css"});
 		chrome.tabs.executeScript({file: "js/vivid.js"});
 	});
 });
