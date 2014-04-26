221337bb574f86854de6e219321b9319cba995f8
RichoHan <Richo@RichoHandeMac-mini.local>
2014-04-26 15:52:48 +0800

    Append note under <body>

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -1,2 +1,5 @@
-var url=window.location.toString();
+var url = window.location.toString();
 console.log(url);
+
+$("body").append("<div class='vivid_note'>test</div>");
+// var pdiv = $( "p" ).add( "div" );
