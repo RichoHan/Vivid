1c3b9012b4d49e0587bab1b206a0c9a9832de124
RichoHan <Richo@RichoHandeMac-mini.local>
2014-04-26 14:17:11 +0800

    Add popup page

--- background.js
+++ background.js
@@ -5,8 +5,10 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
+  console.log('Cover ' + tab.url + ' with new layer');
   chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
+    code: '
+    	document.body.style.backgroundColor="red"
+    '
   });
 });
