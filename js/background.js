421464e89fe2a6ad2b269cde71d6a2dd02ed9251
韓政憲 <richo2192000@gmail.com>
2014-05-14 11:26:52 +0800

    Solve duplicated js issue

--- js/background.js
+++ js/background.js
@@ -1,24 +1,17 @@
+var init = false;
+var state = 0;
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript({file: "js/jquery-2.1.0.min.js"},function () {
 		chrome.tabs.executeScript({file: "js/bootstrap.min.js"},function () {
 			chrome.tabs.insertCSS({file: "stylesheets/styles.css"});
 			chrome.tabs.insertCSS({file: "css/bootstrap.min.css"});
 			chrome.tabs.executeScript({file: "js/vivid.js"});
-			// chrome.tabs.executeScript({file: "js/pic_note.js"});
+			chrome.tabs.executeScript({code: "var init = " + init + ";"},function () {
+				chrome.tabs.executeScript({file: "js/pic_note.js"});
+				state++;
+				if(state==2)
+					init = true;
+			});
 		});
-		// chrome.tabs.executeScript({
-		// 	code: 	'var bkg = chrome.extension.getBackgroundPage();' +
-		// 			'bkg.console.log($("#vivid_note_reformat").length);'
-		// });
-		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-			console.log(tabs);
-		});
-		// var bkg = chrome.extension.getBackgroundPage();
-		// bkg.console.log($('#vivid_note_reformat'));
-		// bkg.console.log($('#vivid_note_reformat').length);
-		// if($('#vivid_note_reformat').length == 0){
-		// 	chrome.tabs.executeScript({file: "js/pic_note.js"});
-		// }
-		// console.log($('#vivid_note_reformat').length);
 	});
 });
--- js/pic_note.js
+++ js/pic_note.js
@@ -1,13 +1,12 @@
-// if($('#vivid_note_reformat').length == 0){
+if(!init){
+	console.log(init);
+
 	$("#vivid_BK").click(function() { 
-		// alert("Test");
-		if($(this).attr("id")=="vivid_BK"){
-			alert("test");
-		} 
+		alert("test");
 	});
 
 	$("#vivid_PN").click(function() {
-		// alert("Picture Note-taking!");
 		console.log("Picture Note-taking!");
 	});
-// }
+
+}
