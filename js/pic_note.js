c81d1aa9e1fe8e2ad1164286283c8a3b5c3cb93e
韓政憲 <richo2192000@gmail.com>
2014-05-25 18:12:21 +0800

    Add popover

--- js/pic_note.js
+++ js/pic_note.js
@@ -1,6 +1,15 @@
 // console.log(init);
 if(!init){
+	// Init variables
+	var show = true;
+	var lock = false;
+	var img = '<button onclick="loadImage()" type="button" class="btn btn-primary">Load</button>';
+	var title = 'Image Search - ';
+	var but_1 = '';
+	var but_2 = '';
+	var but_3 = '';
 
+	// Button events
 	$("#vivid_BK").click(function() { 
 		alert("test");
 	});
@@ -9,12 +18,66 @@ if(!init){
 		console.log("Picture Note-taking!");
 	});
 
-	var p = $('p');
-
+	// Assign each word an event
+	var p = $('.vivid_note_content_p');
 	p
 	.html(function(index, oldHtml) {
-		return oldHtml.replace(/\b(\w+?)\b/g, '<span class="word">$1</span>')
+		return oldHtml.replace(/\b(\w+?)\b/g, '<span class="keyword" id="keyword_$1' + '' + '"" style="cursor:pointer; background:grey;">$1</span>')
 	})
-	.click(function(event) { alert(event.target.innerHTML) });
+	.click(function(event) { 
+		// console.log($1.text());
+		// $(this).popover('show');
+		// alert(event.target.innerHTML) 
+	});
+
+	// Keyword click events
+	var keyword = $('.keyword');
+	keyword
+	.click(function(event) {
+
+	})
+	.popover({title: title+$(this).text(), html: true, content: function () {
+
+		if(show){
+			count = 0;
+			img = '<button id="loadButton" type="button" class="btn btn-success">Load</button>';
+			// onclick="loadImage('+$(this).text()+')"
+			but_1 = '';
+			but_2 = '';
+			but_3 = '';
+		}
+		show = false;
+		lock = true;
+
+		return img + but_1 + but_3 + but_2;
+	}})
+	// .parent().delegate('button#loadButton', 'click', function() {
+	// 	console.log('World!');
+	// });
+
+	.on('shown.bs.popover', function () {
+		var target_word = $(this).text();
+		var loadImage = function(input_txt) {
+	        // alert('loadImage!');
+	        console.log(input_txt);
+	        // img = '<img height="200" src="image/cat.png" />';
+			// but_1 = '';
+			// if(img_link.length==1)
+				// but_2 = '';
+			// else
+			// but_2 = '<button id="r_search" onclick="r_search_img()" type="button" class="btn btn-primary btn-xs" >〉</button>';
+			// but_3 = '<button onclick="tagImage()" type="button" class="btn btn-primary btn-xs" style="margin-right:30px;">Select</button>'
+			// $('#keyword_'+input_txt).popover('hide');
+			// $('#keyword_'+input_txt).popover('show');
+	    };
+
+	    $("#loadButton").click(function(event) {
+	    	loadImage(target_word);
+	    	$('#keyword_'+target_word).popover('hide');
+	    	// $('#keyword_'+target_word).popover('show');
+	    	// $(this).parent().popover('hide');
+	    });
+		// console.log('popover shown!');
+	});
 
 }
--- js/vivid.js
+++ js/vivid.js
@@ -1,19 +1,18 @@
 var url=window.location.toString();
 
-if($('#vivid_note_reformat').length == 0){
+if($('#vivid_note_content').length == 0){
 	// Extract the pure content of the webpage
 	// $.getJSON("https://www.readability.com/api/content/v1/parser?url="+ url +"&token=5a2ddd762b80b65f061bd17c984e61f32b50bdf4",
 	$.getJSON("http://boilerpipe-web.appspot.com/extract?url=" + url + "&output=json",
 		function (data) {
-			console.log(data);
-
+			// console.log(data);
 			var content = data.response.content;
 
 			$('body').append('<div id="vivid_note_reformat"></div>');
 			// Test <p>
 			// $('#vivid_note_reformat').append('<p>123</p>');
 			$('#vivid_note_reformat').append('<div id="vivid_note_content"></div>');
-			$('#vivid_note_content').append('<p>'+content+'</p>');
+			$('#vivid_note_content').append('<p class="vivid_note_content_p">'+content+'</p>');
 			$('#vivid_note_reformat').append('<div id="vivid_note_tool"></div>');
 			$('#vivid_note_tool').append('<button id="vivid_BK" type="button" class="btn btn-primary">Back</button>');
 			$('#vivid_note_tool').append('<button id="vivid_MT" type="button" class="btn btn-primary">Translation</button>');
