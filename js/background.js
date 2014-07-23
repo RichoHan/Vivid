ad3e60d5c7a10a54a10d6ec5830aedb126e80b63
Cheng-Hsien Han <richo2192000@gmail.com>
2014-07-24 01:59:05 +0800

    Reboot

--- js/background.js
+++ js/background.js
@@ -1,44 +1,21 @@
-var init = false;
-var vivid_init = false;
-var pic_note_init = false;
-var state = 0;
-
-localStorage["state"] = 0;
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript({file: "js/jquery-2.1.0.min.js"},function () {
-		chrome.tabs.executeScript({file: "js/bootstrap.min.js"},function () {
-			chrome.tabs.executeScript({file: "js/icheck.min.js"},function () {
-				chrome.tabs.executeScript({file: "js/d3.min.js"},function () {
-					chrome.tabs.executeScript({file: "js/PorterStemmer2.min.js"},function () {
-				// <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
-
-						// then the state # will increase
-						// thus, when extension is executed after the second time,
-						// the pic_note.js will not be inserted again
-						state = parseInt(localStorage["state"]);
-						localStorage["state"] = state + 1;
-						console.log(localStorage["state"]);
-						if(localStorage["state"]>=2)
-							vivid_init = true;
-						if(localStorage["state"]>=3)
-							pic_note_init = true;
-
-						chrome.tabs.insertCSS({file: "stylesheets/styles.css"});
-						chrome.tabs.insertCSS({file: "css/bootstrap.min.css"});
-						chrome.tabs.insertCSS({file: "css/bootflat.min.css"});
-						// chrome.tabs.executeScript({file: "js/vivid.js"});
-						chrome.tabs.executeScript({code: "var pic_note_init = " + pic_note_init + ";" + "var vivid_init = " + vivid_init + ";" + "var state = " + state + ";"},function () {
-							// insert js at the first time
-							// if(!vivid_init)
-							chrome.tabs.executeScript({file: "js/vivid.js"});
-							// if(!pic_note_init)
-							chrome.tabs.executeScript({file: "js/pic_note.js"});
-							
-						});
-					});
-				});
-			});
-		});
-	});
+	chrome.tabs.executeScript({file: "js/jquery-2.1.1.min.js"},function () {
+		$.ajax({
+			type: "GET",
+			url: "http://140.114.200.63/tagging/result",
+			// data: "",
+			// dataType: "text",
+			success: function(data, textStatus) {
+				window.location.href = "google.com";
+				if (data.redirect) {
+					window.location.href = data.redirect;
+				} else {
+					console.log(data);
+				}
+				console.log(data);
+			},
+			error: function(err){
+				console.log(err);
+			}
+		});	});
 });
