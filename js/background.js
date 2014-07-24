c57b3bbcf09308257857398c6ad0ef3bf26fe6de
Richo <richo2192000@gmail.com>
2014-07-24 19:58:13 +0800

    Get urls

--- js/background.js
+++ js/background.js
@@ -2,17 +2,12 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript({file: "js/jquery-2.1.1.min.js"},function () {
 		$.ajax({
 			type: "GET",
-			url: "http://140.114.200.63/tagging/result",
+			url: "http://127.0.0.1:8000/getUrl",
 			// data: "",
 			// dataType: "text",
 			success: function(data, textStatus) {
-				window.location.href = "google.com";
-				if (data.redirect) {
-					window.location.href = data.redirect;
-				} else {
-					console.log(data);
-				}
-				console.log(data);
+				// alert(data.url);
+				window.open(data.url);
 			},
 			error: function(err){
 				console.log(err);
