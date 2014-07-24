8c3d70040fe6e690339b498cb38976c1146fdc66
Richo <richo2192000@gmail.com>
2014-07-24 20:23:53 +0800

    pass current url

--- js/background.js
+++ js/background.js
@@ -1,16 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript({file: "js/jquery-2.1.1.min.js"},function () {
-		$.ajax({
-			type: "GET",
-			url: "http://127.0.0.1:8000/getUrl",
-			// data: "",
-			// dataType: "text",
-			success: function(data, textStatus) {
-				// alert(data.url);
-				window.open(data.url);
-			},
-			error: function(err){
-				console.log(err);
-			}
-		});	});
+    	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    		console.log(tabs[0].url);
+			var current_url = tabs[0].url;
+			$.get('http://127.0.0.1:8000/getUrl', { current_url: current_url }, function(data) {
+    			window.open(data.url);
+			});
+		});
+	});
 });
